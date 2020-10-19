5dc325d68d56feac61581d7685fd471514de2187
root <teppalsateesh@gmail.com>
2020-10-19 12:00:56 +0530

    Version 6

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Hello, World!</h1>'
+    return '<center><h1>Hello, World!</h1></center>'
 
 
 if __name__ == '__main__':
